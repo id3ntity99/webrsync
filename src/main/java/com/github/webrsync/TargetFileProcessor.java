c67ef187382541aefa0f9e523c6e0aae0f305dbb
id3ntity99 <jas06113@gmail.com>
2023-02-08 14:53:22 +0900

    Impled HTTP request handler

--- src/main/java/com/github/webrsync/TargetFileProcessor.java
+++ src/main/java/com/github/webrsync/TargetFileProcessor.java
@@ -1,2 +1,25 @@
-package com.github.webrsync;public class TargetFileProcessor {
+package com.github.webrsync;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+import java.io.File;
+import java.util.Properties;
+
+public class TargetFileProcessor extends SimpleChannelInboundHandler<FullHttpRequest> {
+    private final Properties prop;
+
+    public TargetFileProcessor(Properties prop) {
+        super();
+        this.prop = prop;
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest req) {
+        String reqUri = req.uri().replaceFirst("/", ""); // Remove first separator
+        String targetUri = prop.getProperty("basePath") + reqUri;
+        File targetFile = new File(targetUri);
+        ctx.fireChannelRead(targetFile);
+    }
 }
