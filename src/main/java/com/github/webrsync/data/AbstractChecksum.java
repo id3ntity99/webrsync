384f5cfb661841f37136623b442e3bad0104f6a2
id3ntity99 <jas06113@gmail.com>
2023-02-16 20:46:30 +0900

    fix: Use writeInt() method to move writer index forward

--- src/main/java/com/github/webrsync/data/AbstractChecksum.java
+++ src/main/java/com/github/webrsync/data/AbstractChecksum.java
@@ -3,17 +3,21 @@ package com.github.webrsync.data;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
-public abstract class AbstractChecksum implements Holder<AbstractChecksum>{
+/**
+ * Skeletal implementation of {@link Holder}. This abstract class contains internal {@link ByteBuf} whose reader index is 0.
+ * Instantiation of the subclasses will write data into the buffer, causing the writer index to be non-zero.
+ */
+public abstract class AbstractChecksum implements Holder<AbstractChecksum> {
     protected final ByteBuf buffer;
 
     protected AbstractChecksum(int weakChecksum) {
         buffer = Unpooled.buffer(4, 4);
-        buffer.setInt(0, weakChecksum);
+        buffer.writeInt(weakChecksum);
     }
 
     protected AbstractChecksum(byte[] strongChecksum) {
         buffer = Unpooled.buffer(16, 16);
-        buffer.setBytes(0, strongChecksum);
+        buffer.writeBytes(strongChecksum);
     }
 
     public ByteBuf content() {
