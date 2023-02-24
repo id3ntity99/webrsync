7fab9544b1d489211fc0c67f72a7cfd880a8412d
id3ntity99 <jas06113@gmail.com>
2023-02-24 13:46:59 +0900

    refact: Optimized imports

--- src/main/java/com/github/webrsync/bootstrap/ChannelInitializerImpl.java
+++ src/main/java/com/github/webrsync/bootstrap/ChannelInitializerImpl.java
@@ -1,15 +1,9 @@
 package com.github.webrsync.bootstrap;
 
-import com.github.webrsync.ChecksumHolderProcessor;
-import com.github.webrsync.ChecksumParser;
-import com.github.webrsync.TargetFileProcessor;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
-
-import java.io.InputStream;
-import java.util.Properties;
 
 final class ChannelInitializerImpl extends ChannelInitializer<Channel> {
     private final ChannelHandler[] handlers;
@@ -22,7 +16,7 @@ final class ChannelInitializerImpl extends ChannelInitializer<Channel> {
     protected void initChannel(Channel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
         for (ChannelHandler handler : handlers) {
-           pipeline.addLast(handler);
+            pipeline.addLast(handler);
         }
     }
 }
