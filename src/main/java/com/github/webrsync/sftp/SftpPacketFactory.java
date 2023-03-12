a63c4a0d651a761864722388e22d54a6284c657d
id3ntity99 <jas06113@gmail.com>
2023-03-12 22:39:24 +0900

    feat: Abstract factory class for easy-instantiating of SFTP packets

---
+++ src/main/java/com/github/webrsync/sftp/SftpPacketFactory.java
@@ -0,0 +1,18 @@
+package com.github.webrsync.sftp;
+
+public class SftpPacketFactory {
+    public SftpPacket initPacket() {
+        //TODO Impl this
+        return null;
+    }
+
+    public SftpPacket versionPacket() {
+        //TODO Impl this
+        return null;
+    }
+
+    public FullSftpPacket newPacket() {
+        //TODO Impl this
+        return null;
+    }
+}
