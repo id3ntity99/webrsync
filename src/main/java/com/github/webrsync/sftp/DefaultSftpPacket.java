8b508b255b903a5dfbebf565602de15dbffdd0f8
id3ntity99 <jas06113@gmail.com>
2023-03-16 21:59:09 +0900

    feat: Implemented ByteBufHolder methods

--- src/main/java/com/github/webrsync/sftp/DefaultSftpPacket.java
+++ src/main/java/com/github/webrsync/sftp/DefaultSftpPacket.java
@@ -1,16 +1,16 @@
 package com.github.webrsync.sftp;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.DefaultByteBufHolder;
 
-public class DefaultSftpPacket extends DefaultByteBufHolder implements SftpPacket {
+public class DefaultSftpPacket implements SftpPacket {
     private final int length;
-    private final SftpPacketType type;
+    protected final SftpPacketType type;
+    protected final ByteBuf content;
 
-    public DefaultSftpPacket(SftpPacketType type, ByteBuf payload) {
-        super(payload);
+    public DefaultSftpPacket(SftpPacketType type, ByteBuf content) {
+        this.content = content;
         this.type = type;
-        this.length = 1 + payload.readableBytes(); // length of the packet as byte, excluding length field itself.
+        this.length = 1 + content.readableBytes(); // length of the packet as byte, excluding length field itself.
     }
 
     @Override
@@ -21,5 +21,71 @@ public class DefaultSftpPacket extends DefaultByteBufHolder implements SftpPacke
     @Override
     public SftpPacketType type() {
         return type;
+    }
+
+    @Override
+    public ByteBuf content() {
+        return content;
+    }
+
+    @Override
+    public SftpPacket copy() {
+        ByteBuf copied = content.copy();
+        return new DefaultSftpPacket(type, copied);
+    }
+
+    @Override
+    public SftpPacket duplicate() {
+        ByteBuf duplicated = content.duplicate();
+        return new DefaultSftpPacket(type, duplicated);
+    }
+
+    @Override
+    public SftpPacket replace(ByteBuf content) {
+        return new DefaultSftpPacket(type, content);
+    }
+
+    @Override
+    public SftpPacket retain() {
+        content.retain();
+        return this;
+    }
+
+    @Override
+    public SftpPacket retain(int increment) {
+        content.retain(increment);
+        return this;
+    }
+
+    @Override
+    public SftpPacket retainedDuplicate() {
+        return null;
+    }
+
+    @Override
+    public SftpPacket touch() {
+        content.touch();
+        return this;
+    }
+
+    @Override
+    public SftpPacket touch(Object hint) {
+        content.touch(hint);
+        return this;
+    }
+
+    @Override
+    public int refCnt() {
+        return content.refCnt();
+    }
+
+    @Override
+    public boolean release() {
+        return content.release();
+    }
+
+    @Override
+    public boolean release(int decrement) {
+        return content.release(decrement);
     }
 }
