3f21e0e328e6ac8ccfcd5585904b9671b0102bc5
id3ntity99 <jas06113@gmail.com>
2023-07-07 18:06:34 +0900

    remove: Deletion of deprecated classes

--- src/main/java/com/github/webrsync/WrongAlgorithmException.java
+++
@@ -1,8 +0,0 @@
-package com.github.webrsync;
-
-@Deprecated
-public class WrongAlgorithmException extends RuntimeException {
-    public WrongAlgorithmException(String s, Throwable t) {
-        super(s, t);
-    }
-}
--- src/main/java/com/github/webrsync/data/OffsetLimitException.java
+++
@@ -1,9 +0,0 @@
-package com.github.webrsync.data;
-
-@Deprecated
-public class OffsetLimitException extends RuntimeException {
-    public OffsetLimitException(int currentOffset, int offsetLimit) {
-        super("Incrementing from current offset exceeds the offset limit. " +
-                "current offset = " + currentOffset + " , offset-limit = " + offsetLimit);
-    }
-}
