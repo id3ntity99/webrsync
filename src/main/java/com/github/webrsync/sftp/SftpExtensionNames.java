c1e664a00ce15ee66ef6ab87a04aaaea78c8307f
id3ntity99 <jas06113@gmail.com>
2023-07-07 18:01:19 +0900

    feat: A hash table for SFTP attributes(extensions, file attributes)

---
+++ src/main/java/com/github/webrsync/sftp/SftpExtensionNames.java
@@ -0,0 +1,9 @@
+package com.github.webrsync.sftp;
+
+public class SftpExtensionNames {
+    public static final String VERSIONS = "VERSIONS";
+    public static final String VERSION_SELECT = "VERSION_SELECT";
+    public static final String FILENAME_CHARSET = "FILENAME_CHARSET";
+
+    private SftpExtensionNames(){};
+}
