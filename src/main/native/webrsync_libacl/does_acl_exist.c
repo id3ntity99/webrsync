fd76b829d0fef5f49423b41266b9f46f5eaa91b5
id3ntity99 <jas06113@gmail.com>
2023-12-08 20:14:30 +0900

    feat: Function for checking if ACL exist

--- src/main/native/webrsync_libacl/does_acl_exist.c
+++ src/main/native/webrsync_libacl/does_acl_exist.c
@@ -2,9 +2,17 @@
 // Created by lee on 12/8/23.
 //
 
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/errno.h>
 #include <sys/xattr.h>
 #include "sftp_acl.h"
 
-extern int does_acl_exist(const char *path) {
+extern ssize_t does_acl_exist(const char *path) {
+    ssize_t read = listxattr(path, NULL, 0);
+    if (read == -1) {
+        char *msg = "Someting went wrong";
+        throw_exception_errno(__func__, __LINE__, __FILE__, errno, msg);
+    }
+    return read;
 }
