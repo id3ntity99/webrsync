3eca0db706c461818f0699ff4d25871828127871
id3ntity99 <jas06113@gmail.com>
2023-12-08 22:49:00 +0900

    fix: Coloring err message prints gibberish, deleted coloring feat

--- src/main/native/webrsync_libacl/throw_exception.c
+++ src/main/native/webrsync_libacl/throw_exception.c
@@ -4,16 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-
 extern void throw_exception(const char *func_name, int lineno, char *file_name, char *msg) {
     printf("\n");
-    printf(RED "[NATIVE] Exception in \"%s\" (%s:%d):\n" RESET, func_name, file_name, lineno);
-    printf(RED "[NATIVE] %s\n" RESET, msg);
+    printf("[NATIVE] Exception in \"%s\" (%s:%d):\n", func_name, file_name, lineno);
+    printf("[NATIVE] %s\n", msg);
 }
 
 extern void throw_exception_errno(const char *func_name, int lineno, char *file_name, int err_num, char *msg) {
     throw_exception(func_name, lineno, file_name, msg);
-    printf(RED "[NATIVE] %s(status code = %d)\n" RESET, strerror(err_num), err_num);
+    printf("[NATIVE] %s(status code = %d)\n", strerror(err_num), err_num);
 }
