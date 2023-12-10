9697a8a1eee2b129cee9513ff60acb8ed421f7a4
id3ntity99 <jas06113@gmail.com>
2023-12-10 22:05:55 +0900

    refact: Use NoSuchFileException instead

--- src/main/java/com/github/webrsync/sftp/fs/AclManager.java
+++ src/main/java/com/github/webrsync/sftp/fs/AclManager.java
@@ -4,19 +4,21 @@ import com.github.webrsync.sftp.internal.LibLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.NoSuchFileException;
+
 public class AclManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(AclManager.class);
 
     static {
         try {
             LibLoader.load("acl");
-        } catch (NoSuchFieldException e) {
-            LOGGER.debug(e.getMessage(), e);
+        } catch (NoSuchFileException e) {
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
     private AclManager() {
-        //Helper class does not have constructor
+        //Utility class does not have constructor
     }
 
     public static native int setSftpAcl(String path, AccessControlList acl, SetXattrFlag flag);
--- src/main/java/com/github/webrsync/sftp/internal/LibLoader.java
+++ src/main/java/com/github/webrsync/sftp/internal/LibLoader.java
@@ -6,12 +6,13 @@ import org.slf4j.LoggerFactory;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.NoSuchFileException;
 import java.util.Properties;
 
 public class LibLoader {
     private static final Logger logger = LoggerFactory.getLogger(LibLoader.class);
 
-    public static void load(String libName) throws NoSuchFieldException {
+    public static void load(String libName) throws NoSuchFileException {
         Properties props = new Properties();
         String path = System.getProperty("user.dir") + "/src/main/resources/app.properties";
         try (InputStream in = new FileInputStream(path)) {
@@ -23,7 +24,7 @@ public class LibLoader {
                 String libPath = props.getProperty("libastat.dev");
                 System.load(libPath);
             } else {
-                throw new NoSuchFieldException("There is no such field in app.properties: " + libName);
+                throw new NoSuchFileException("There is no such field in app.properties: " + libName);
             }
         } catch (IOException e) {
             logger.error("Exception raised while getting path of app.properties", e);
