c5d30f7978160663333864218a28e9d1b7f1fbfe
id3ntity99 <jas06113@gmail.com>
2023-12-10 22:00:04 +0900

    refact: Refactoring on progress

--- src/main/java/com/github/webrsync/sftp/attrib/AttribBits.java
+++ src/main/java/com/github/webrsync/sftp/attrib/AttribBits.java
@@ -1,6 +1,6 @@
 package com.github.webrsync.sftp.attrib;
 
-public class AttribBits {
+public final class AttribBits {
     public static final AttribBits READ_ONLY = new AttribBits(0x00000001);
     public static final AttribBits SYSTEM = new AttribBits(0x00000002);
     public static final AttribBits HIDDEN = new AttribBits(0x00000004);
@@ -13,15 +13,12 @@ public class AttribBits {
     public static final AttribBits IMMUTABLE = new AttribBits(0x00000200);
     public static final AttribBits SYNC = new AttribBits(0x00000400);
     public static final AttribBits TRANSLATION_ERR = new AttribBits(0x00000800);
-    private static final int SIZE = 12;
-    private static int cnt = 0;
+    private static final int mask = 0x00000000;
     private final int value;
-    private final int[] validBits = new int[SIZE];
 
+    //TODO: attrib-bits-valid should be the property of this class.
     private AttribBits(int value) {
         this.value = value;
-        validBits[cnt] = value;
-        cnt++;
     }
 
     public int value() {
