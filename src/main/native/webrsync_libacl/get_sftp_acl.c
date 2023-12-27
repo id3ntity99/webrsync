35ee9660317a47c666fd1286f25efcacea4c5704
id3ntity99 <jas06113@gmail.com>
2023-12-28 00:14:30 +0900

    chores: Modified err msg

--- src/main/native/webrsync_libacl/get_sftp_acl.c
+++ src/main/native/webrsync_libacl/get_sftp_acl.c
@@ -11,15 +11,20 @@
 extern size_t get_xattr_size(const char *path) {
     ssize_t read = getxattr(path, SFX_ACL_XATTR_NAME, NULL, 0);
     if (read == -1) {
-        char *msg = "Something went wrong";
+        char *msg = "Something went wrong while getting xattr";
         throw_exception_errno(__func__, __LINE__, __FILE__, errno, msg);
     }
     return read;
 }
 
 extern int get_sftp_acl(const char *path, unsigned char *buf) {
-    if (buf == NULL || path == NULL) {
-        char *msg = "Either const char *path or unsigned char *buf can be NULL.\n";
+    if (buf == NULL) {
+        char *msg = "unsigned char *buf cannot be NULL";
+        throw_exception(__func__, __LINE__, __FILE__, msg);
+        return -1;
+    }
+    if (path == NULL) {
+        char *msg = "const char *path cannot be NULL";
         throw_exception(__func__, __LINE__, __FILE__, msg);
         goto free_failed;
     }
@@ -27,7 +32,7 @@ extern int get_sftp_acl(const char *path, unsigned char *buf) {
     ssize_t read = getxattr(path, SFX_ACL_XATTR_NAME, buf, len);
     if (read == -1) {
         char *msg = "Something went wrong while getting an ACL";
-        throw_exception_errno(__func__ , __LINE__, __FILE__, errno, msg);
+        throw_exception_errno(__func__, __LINE__, __FILE__, errno, msg);
         goto free_failed;
     }
     return 0;
